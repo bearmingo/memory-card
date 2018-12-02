098a72c9f60f1aece537b662cbaa36977f5c9660
bearmingo <bearmingo@icloud.com>
2018-12-02 11:12:08 +0800

    Initial Commit

---
+++ memory-cardTests/memory_cardTests.swift
@@ -0,0 +1,34 @@
+//
+//  memory_cardTests.swift
+//  memory-cardTests
+//
+//  Created by 陈国民 on 2018/12/2.
+//  Copyright © 2018 陈国民. All rights reserved.
+//
+
+import XCTest
+@testable import memory_card
+
+class memory_cardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
